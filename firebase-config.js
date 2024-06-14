4db636f2ed51f5854a462a6e3e044e2ce2f28cbb
Misael Tapia <misaeltapiac71@gmail.com>
2024-06-14 19:52:03 -0400

    :package: :heavy_plus_sign: Se ha agregado expo-image-picker y :wrench: se ha configurado Firestore y Storage

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -7,3 +9,6 @@ const firebaseConfig = JSON.parse(process.env.EXPO_PUBLIC_FIREBASE_CONFIG);
 
 const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {persistence: getReactNativePersistence(ReactNativeAsyncStorage)});
+
+export const db = getFirestore(app);
+export const storage = getStorage();
