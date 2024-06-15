91da74168ed084f85cb617a9e5901a482a0c499b
Desi <141796712+DesiList753@users.noreply.github.com>
2024-06-15 17:47:11 -0400

    :construction_worker: agregado checkbox de recordar sesion

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,10 +1,12 @@
 import { Text, View, ActivityIndicator } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function LoadingScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text style={{ marginBottom: 20 }}>ECB-FIS</Text>
             <ActivityIndicator size="large" color="#0000ff" />
+            <StatusBar style="auto" />
         </View>
     );
 }
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,129 +1,173 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, BackHandler } from 'react-native';
-import { useEffect, useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  BackHandler,
+} from "react-native";
+import { useEffect, useState } from "react";
+import Checkbox from "expo-checkbox";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { auth } from '../firebase-config'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from "../firebase-config";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const errorMessages = {
-    'auth/email-already-in-use': 'La dirección de correo electrónico ya está en uso',
-    'auth/invalid-email': 'La dirección de correo electrónico no es válida',
-    'auth/weak-password': 'La contraseña es muy débil',
-    'auth/missing-password': 'Ingrese una contraseña',
+  "auth/email-already-in-use":
+    "La dirección de correo electrónico ya está en uso",
+  "auth/invalid-email": "La dirección de correo electrónico no es válida",
+  "auth/weak-password": "La contraseña es muy débil",
+  "auth/missing-password": "Ingrese una contraseña",
 };
 
 export default function Login({ navigation }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
 
-    // Salir de la aplicación.
-    useEffect(() => {
-      const backAction = () => {
-        Alert.alert('Salir', '¿Está seguro que desea salir de la aplicación?', [
-          { text: 'Cancelar', style: 'cancel', onPress: () => null },
-          { text: 'Salir', onPress: () => BackHandler.exitApp() }
-        ]);
-        return true;
-      };
-      const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
-      return () => backHandler.remove();
-    }, []);
-
-    // Crear cuenta
-    const handleCreateAccount = () => {
-      createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        // console.log(user);
-        Alert.alert('Exito', 'Cuenta creada con exito');
-      })
-      .catch((error) => {
-        Alert.alert('Error', errorMessages[error.code] || error.message);
-      })
-    }
-    // Iniciar sesion
-    const handleLogin = () => {
-      signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        // console.log(user);
-        Alert.alert('Exito', 'Sesion iniciada con exito');
-      })
-      .catch((error) => {
-        Alert.alert('Error', errorMessages[error.code] || error.message);
-      })
-    }
-
-    return (
-      <View style={styles.container}>
-        <Text style={[styles.title, {marginBottom: 100}]}>ECB-FIS</Text>
-
-        <Text style={styles.label}>Correo electrónico</Text>
-        <TextInput onChangeText={text => setEmail(text)} style={[styles.input, {marginBottom: 20}]} placeholder='email@dominio.com'/>
-        
-        <Text style={styles.label} >Contraseña</Text>
-        <TextInput onChangeText={text => setPassword(text)} style={[styles.input, {marginBottom: 40}]} placeholder='******' secureTextEntry/>
-
-        <TouchableOpacity onPress={handleLogin} style={styles.button}>
-          <Text style={{color: 'white', fontWeight: 'bold'}}>Iniciar Sesión</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={handleCreateAccount} style={styles.button}>
-          <Text style={{color: 'white', fontWeight: 'bold'}}>Crear Cuenta</Text>
-        </TouchableOpacity>
-
-        <Text style={[styles.message]}>Al crear una cuenta, aceptas nuestros <Text style={styles.messageBlack}>Términos de Servicio</Text> y <Text style={styles.messageBlack}>Política de Privacidad</Text>.</Text>
-        
-        <StatusBar style="auto" />
-      </View>
+  // Salir de la aplicación.
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert("Salir", "¿Está seguro que desea salir de la aplicación?", [
+        { text: "Cancelar", style: "cancel", onPress: () => null },
+        { text: "Salir", onPress: () => BackHandler.exitApp() },
+      ]);
+      return true;
+    };
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
     );
-  }
+    return () => backHandler.remove();
+  }, []);
+
+  // Crear cuenta
+  const handleCreateAccount = async () => {
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        // console.log(user);
+        Alert.alert("Exito", "Cuenta creada con exito");
+      })
+      .catch((error) => {
+        Alert.alert("Error", errorMessages[error.code] || error.message);
+      });
+  };
+  // Iniciar sesion
+  const handleLogin = async () => {
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        // console.log(user);
+        Alert.alert("Exito", "Sesion iniciada con exito");
+        if (!rememberMe) {
+          AsyncStorage.clear();
+        }
+      })
+      .catch((error) => {
+        Alert.alert("Error", errorMessages[error.code] || error.message);
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.title, { marginBottom: 100 }]}>ECB-FIS</Text>
+
+      <Text style={styles.label}>Correo electrónico</Text>
+      <TextInput
+        onChangeText={(text) => setEmail(text)}
+        style={[styles.input, { marginBottom: 20 }]}
+        placeholder="email@dominio.com"
+      />
+
+      <Text style={styles.label}>Contraseña</Text>
+      <TextInput
+        onChangeText={(text) => setPassword(text)}
+        style={[styles.input, { marginBottom: 20 }]}
+        placeholder="******"
+        secureTextEntry
+      />
+
+      <View style={styles.checkboxContainer}>
+        <Checkbox value={rememberMe} onValueChange={setRememberMe} />
+        <Text style={styles.checkboxLabel}>Recordar sesión</Text>
+      </View>
+
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
+        <Text style={{ color: "white", fontWeight: "bold" }}>
+          Iniciar Sesión
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={handleCreateAccount} style={styles.button}>
+        <Text style={{ color: "white", fontWeight: "bold" }}>Crear Cuenta</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.message]}>
+        Al crear una cuenta, aceptas nuestros{" "}
+        <Text style={styles.messageBlack}>Términos de Servicio</Text> y{" "}
+        <Text style={styles.messageBlack}>Política de Privacidad</Text>.
+      </Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-    title: {
-        fontSize: 40,
-        fontWeight: 'bold',
-    },
-
-    label: {
-        // fontSize: 12,
-        fontWeight: 'bold',
-    },
-
-    input: {
-        height: 40,
-        width: 300,
-        padding: 10,
-        marginTop: 5,
-        borderWidth: 1,
-        borderColor: '#E0E0E0',
-        borderRadius: 10,
-    },
-
-    button: {
-        height: 40,
-        width: 300,
-        padding: 10,
-        backgroundColor: '#000',
-        borderRadius: 10,
-        alignItems: 'center',
-        marginBottom: 20,
-    },
-
-    message:{
-        color: '#828282',
-        fontSize: 12,
-        width: 325,
-        textAlign: 'center',
-    },
-    messageBlack:{
-        color: '#000',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  label: {
+    // fontSize: 12,
+    fontWeight: "bold",
+  },
+  input: {
+    height: 40,
+    width: 300,
+    padding: 10,
+    marginTop: 5,
+    borderWidth: 1,
+    borderColor: "#E0E0E0",
+    borderRadius: 10,
+  },
+  button: {
+    height: 40,
+    width: 300,
+    padding: 10,
+    backgroundColor: "#000",
+    borderRadius: 10,
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  message: {
+    color: "#828282",
+    fontSize: 12,
+    width: 325,
+    textAlign: "center",
+  },
+  messageBlack: {
+    color: "#000",
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  checkboxLabel: {
+    marginLeft: 8,
+  },
 });
