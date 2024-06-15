9d9159d4fa73a2644bfd271f54c2f602ae389a26
Misael Tapia <misaeltapiac71@gmail.com>
2024-06-15 18:26:04 -0400

    :heavy_plus_sign: Se ha agregado react-native-picker/picker, :construction: se ha actualizado el formData

--- screens/UploadScreen.js
+++ screens/UploadScreen.js
@@ -1,30 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, View, StyleSheet, TouchableOpacity, TextInput, ScrollView, Text, Alert } from 'react-native';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import { auth } from '../firebase-config';
 import { agregarDocumento, subirArchivo } from '../utils/firebaseUtils';
 import { seleccionarImagen } from '../utils/seleccionarImagen';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Picker } from '@react-native-picker/picker';
 
 export default function UploadScreen() {
     const [loading, setLoading] = useState(false);
+    const [ejercicioImg, setEjercicioImg] = useState(null);
+    const [formulaImg, setFormulaImg] = useState(null);
+
     const [formData, setFormData] = useState({
+        titulo: '', // Título del ejercicio.
         descripcion: '', // De que trata el ejercicio.
         materia: '', // Materia a la que pertenece el ejercicio.
         contenido: '', // Enunciado del ejercicio.
-        imagenURL: '', // URL de la imagen del ejercicio.
-        formulaURL: '', // URL de la formula del ejercicio.
-        respuestas: [], // Posibles respuestas del ejercicio.
+        imagenURL: null, // URL de la imagen del ejercicio.
+        formulaURL: null, // URL de la formula del ejercicio.
+        respuestas: '', // Posibles respuestas del ejercicio.
         respuesta: '', // Respuesta del ejercicio.
+        dificultad: 'Medio', // Dificultad del ejercicio.
         puntaje: 0, // Puntaje del ejercicio.
-        tipoEjercicio: '', // Ejercicio "Diario" o "Normal".
+        tipoEjercicio: 'Normal', // Ejercicio "Diario" o "Normal".
         tiempo: 0, // Tiempo en segundos para resolver el ejercicio.
+        retroalimentacion: '', // Retroalimentación del ejercicio.
         publicadoPor: '', // UID del usuario que publicó el ejercicio.
     });
-
-    const [ejercicioImg, setEjercicioImg] = useState(null);
-    const [formulaImg, setFormulaImg] = useState(null);
 
     // Seleccionar una imagen de la galería.
     const handleSeleccionarImagenEjercicio = async () => {
@@ -39,40 +42,46 @@ export default function UploadScreen() {
             setFormulaImg(result);
         }
     };
-
-    // Subir la imagen a Firebase Storage.
+    // Actualizar el estado del formulario.
     const handleInputChange = (name, value) => {
         setFormData({ ...formData, [name]: value });
     };
-
+    // Enviar el formulario a la base de datos.
     const handleSubmit = async () => {
-        setLoading(true);
-        const imagenRef = await subirArchivo(ejercicioImg, 'ejercicios');
-        const formulaRef = await subirArchivo(formulaImg, 'ejercicios-formulas');
-        
-        const exerciseData = {
-            ...formData,
-            respuestas: formData.respuestas.split(',').map(r => r.trim()), // Convertir la cadena de respuestas en un array.
-            puntaje: parseInt(formData.puntaje, 10),
-            tiempo: parseInt(formData.tiempo, 10),
-            publicadoPor: auth.currentUser.uid,
-            imagenURL: imagenRef,
-            formulaURL: formulaRef,
-        };
-    
-        try {
+        try{
+            setLoading(true);
+            const imagenRef = await subirArchivo(ejercicioImg, 'ejercicios');
+            const formulaRef = await subirArchivo(formulaImg, 'ejercicios-formulas');
+            
+            const exerciseData = {
+                ...formData,
+                respuestas: formData.respuestas.split(',').map(r => r.trim()), // Convertir la cadena de respuestas en un array.
+                puntaje: parseInt(formData.puntaje, 10),
+                tiempo: parseInt(formData.tiempo, 10),
+                publicadoPor: auth.currentUser.uid,
+                imagenURL: imagenRef,
+                formulaURL: formulaRef,
+            };
+
             const docId = await agregarDocumento("ejercicios", exerciseData);
             console.log('Ejercicio añadido con ID: ', docId);
             Alert.alert('Exito', `Ejercicio con id ${docId} añadido con exito`);
         } catch (error) {
             console.error('Error añadiendo el ejercicio: ', error);
-            Alert.alert('Error', 'Error añadiendo el ejercicio', error.message);
+            Alert.alert('Ups!', 'Error añadiendo el ejercicio: ', error.message);
         }
         setLoading(false);
     };
 
     return (
         <ScrollView style={styles.container}>
+            <Text style={styles.label}>Título (opcional)</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Título del ejercicio"
+                value={formData.titulo}
+                onChangeText={text => handleInputChange('titulo', text)}
+            />
             <Text style={styles.label}>Descripción (opcional)</Text>
             <TextInput
                 style={styles.input}
@@ -115,18 +124,31 @@ export default function UploadScreen() {
             <Text style={styles.label}>Respuesta</Text>
             <TextInput
                 style={styles.input}
-                placeholder="Respuesta"
+                placeholder="Respuesta correcta"
                 value={formData.respuesta}
                 onChangeText={text => handleInputChange('respuesta', text)}
             />
-            <Text style={styles.label}>Respuestas</Text>
+            <Text style={styles.label}>Respuestas (opcional)</Text>
+            <Text style={{color: '#aaa'}}>En caso de ser un ejercicio de alternativas, ingrese sus respuestas (tambien debe ingresar la respuesta correcta y debe coincidir con el de arriba).</Text>
             <TextInput
                 style={styles.input}
-                placeholder="Respuestas (separadas por coma)"
+                placeholder="Posibles respuestas (separadas por coma)"
                 value={formData.respuestas}
                 onChangeText={text => handleInputChange('respuestas', text)}
             />
-            <Text style={styles.label}>Puntaje</Text>
+            <Text style={styles.label}>Dificultad</Text>
+            <View style={styles.pickerContainer}>
+                <Picker
+                    selectedValue={formData.dificultad}
+                    style={styles.picker}
+                    onValueChange={(itemValue, itemIndex) => handleInputChange('dificultad', itemValue)}
+                >
+                    <Picker.Item label="Fácil" value="Fácil" />
+                    <Picker.Item label="Medio" value="Medio" />
+                    <Picker.Item label="Difícil" value="Difícil" />
+                </Picker>
+            </View>
+            <Text style={styles.label}>Puntaje (número entero)</Text>
             <TextInput
                 style={styles.input}
                 placeholder="Puntaje"
@@ -135,13 +157,17 @@ export default function UploadScreen() {
                 onChangeText={text => handleInputChange('puntaje', text)}
             />
             <Text style={styles.label}>Tipo de Ejercicio</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="'Diario' o 'Normal'"
-                value={formData.tipoEjercicio}
-                onChangeText={text => handleInputChange('tipoEjercicio', text)}
-            />
-            <Text style={styles.label}>Tiempo</Text>
+            <View style={styles.pickerContainer}>
+                <Picker
+                    selectedValue={formData.tipoEjercicio}
+                    style={styles.picker}
+                    onValueChange={(itemValue, itemIndex) => handleInputChange('tipoEjercicio', itemValue)}
+                >
+                    <Picker.Item label="Diario" value="Diario" />
+                    <Picker.Item label="Normal" value="Normal" />
+                </Picker>
+            </View>
+            <Text style={styles.label}>Tiempo (en segundos) (opcional)</Text>
             <TextInput
                 style={styles.input}
                 placeholder="Tiempo (en segundos)"
@@ -149,7 +175,14 @@ export default function UploadScreen() {
                 keyboardType="numeric"
                 onChangeText={text => handleInputChange('tiempo', text)}
             />
-            <TouchableOpacity onPress={handleSubmit} style={[styles.button, {width: '100%'}]} disabled={loading}>
+            <Text style={styles.label}>Retroalimentación (opcional)</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Retroalimentación del ejercicio"
+                value={formData.retroalimentacion}
+                onChangeText={text => handleInputChange('retroalimentacion', text)}
+            />
+            <TouchableOpacity onPress={handleSubmit} style={[!loading ? (styles.button):(styles.buttonDisabled), {width: '100%'}]} disabled={loading}>
                 <Text style={{color: 'white', fontWeight: 'bold'}}>{!loading ? ('Enviar a la base de datos'):('Enviando...') }</Text>
             </TouchableOpacity>
             <Text>
@@ -189,6 +222,22 @@ const styles = StyleSheet.create({
         borderColor: '#E0E0E0',
         borderRadius: 10,
     },
+    pickerContainer: {
+        height: 40,
+        width: '100%',
+        padding: 10,
+        marginTop: 5,
+        marginBottom: 20,
+        borderWidth: 1,
+        borderColor: '#E0E0E0',
+        borderRadius: 10,
+        overflow: 'hidden',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    picker: {
+        width: '100%',
+    },
     button: {
         height: 40,
         width: 150,
@@ -203,7 +252,7 @@ const styles = StyleSheet.create({
         height: 40,
         width: 150,
         padding: 10,
-        backgroundColor: '#0f0f0f',
+        backgroundColor: '#aaa',
         borderRadius: 10,
         alignItems: 'center',
         marginBottom: 20,
